50a9b8a1837c131c974493a0f293e76183aa930a
SuperTurban <tambet.uutsalu@gmail.com>
2017-02-26 22:21:43 +0200

    test

--- client/public/js/app.js
+++ client/public/js/app.js
@@ -1,22 +1,13 @@
 $(function(){
-	$.getJSON('/facts', function(data){
-		$("#skoor").html(data.like_count);
-		$(".fact-in").html(data.poster);
-	},'json');
-
+$("#upvote").addClass('disabled');
 	$(".showcomments").click(function(){
 		$("#comments").toggle();
 	});
 
 	$("#upvote").click(function(){
-		var that = this;
-		$(that).attr('disabled',true);
-		$(that).addClass('disabled');
+		
+		
 
-		$.post('/upvote', function(){
-			$(that).html("ok");
-			$("#skoor").html(Number($("#skoor").html())+1);
-		});
 	})
 });
 
