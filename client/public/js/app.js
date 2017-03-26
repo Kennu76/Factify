ddb3e6e2b9a8158a6c2eead05d0457b749554b7b
Kennu76 <kevin.kanarbik@gmail.com>
2017-03-26 22:31:06 +0300

    Facebook change

--- client/public/js/app.js
+++ client/public/js/app.js
@@ -47,3 +47,14 @@ function showMessage(message){
     elem.fadeOut();
   }, 5000);
 }
+
+FB.login(function(response) {
+     if (response.authResponse) {
+      console.log('Welcome!  Fetching your information.... ');
+      FB.api('/me', function(response) {
+        console.log('Good to see you, ' + response.name + '.');
+      });
+     } else {
+      console.log('User cancelled login or did not fully authorize.');
+     }
+ });
