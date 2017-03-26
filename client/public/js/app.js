c25eaafe8d05dc96682c63806379509ad44db66c
Kennu76 <kevin.kanarbik@gmail.com>
2017-03-26 22:59:04 +0300

    Facebook change

--- client/public/js/app.js
+++ client/public/js/app.js
@@ -47,37 +47,3 @@ function showMessage(message){
     elem.fadeOut();
   }, 5000);
 }
-window.fbAsyncInit = function() {
-    FB.init({
-          appId      : '202083643612448',
-          xfbml      : true,
-          version    : 'v2.8'
-    });
-
-    $(document).trigger('fbload');  //  <---- THIS RIGHT HERE TRIGGERS A CUSTOM EVENT CALLED 'fbload'
-};
-$(document).on(
-    'fbload',  //  <---- HERE'S OUR CUSTOM EVENT BEING LISTENED FOR
-    function(){
-        //some code that requires the FB object
-        //such as...
-        FB.getLoginStatus(function(res){
-            if( res.status == "connected" ){
-                FB.api('/me', function(fbUser) {
-                    console.log("Open the pod bay doors, " + fbUser.name + ".");
-                });
-            }
-        });
-
-    }
-);
-FB.login(function(response) {
-     if (response.authResponse) {
-      console.log('Welcome!  Fetching your information.... ');
-      FB.api('/me', function(response) {
-        console.log('Good to see you, ' + response.name + '.');
-      });
-     } else {
-      console.log('User cancelled login or did not fully authorize.');
-     }
- });
