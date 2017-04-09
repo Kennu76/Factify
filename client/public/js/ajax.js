0a19fcf10a5e22f568fbe1b883aaf29c2f09aa0e
nuubis <marttikakk@hotmail.com>
2017-04-09 14:31:51 +0300

    still AJAX

--- client/public/js/ajax.js
+++ client/public/js/ajax.js
@@ -10,58 +10,16 @@ window.onload = function(){
 function saadaAjax() {
     $("#factcontent").load("/fact/next", function (responseTxt, statusTxt, xhr) {
         if (statusTxt == "success") {
-            //parser = new JSONParser();
-            //json = (JSONObject) parser.parse(stringToParse);
-            //console.log(json);
-            //alert("External content loaded successfully!");
-            //console.log(responseTxt);
-            var response = responseTxt.split(":");
-            //console.log(response);
-
-            // fact
-            var factR = response[1].split(",");
-            var factRe = factR[0].split("#");
-            var factRes = factRe[1].substring(2, (factRe[1].length - 1));
-
-            // Username
-            var usernameR = response[2].split(",");
-            var username = usernameR[0].substring(1, usernameR[0].length - 1);
-
-            // Upvote nr
-            var upvote = response[4].split(",");
-            if (upvote[0].length == 4) {
-                upvote = upvote[0].substring(0, 3);
-            }
-            else if (upvote[0].length == 3) {
-                upvote = upvote[0].substring(0, 2);
-            }
-            else {
-                upvote = upvote[0].substring(0, 1);
-            }
-
-            // Downvote nr
-            var downvote;
-            if (response[5].length == 4) {
-                downvote = response[5].substring(0, 3);
-            }
-            else if (response[5].length == 3) {
-                downvote = response[5].substring(0, 2);
-            }
-            else {
-                downvote = response[5].substring(0, 1);
-            }
+            var info = JSON.parse(responseTxt);
             var currentURL = window.location;
+            currentURL.hash = ""+info.fact;
 
-            currentURL.hash = "#Fact: " + factRes;
-
-            //console.log(response);
-            //console.log(factRes);
-            document.getElementById("factcontent").innerHTML = factRes;
-            document.getElementById("upvotecount").innerHTML = upvote;
-            document.getElementById("downvotecount").innerHTML = downvote;
-            document.getElementById("factusername").innerHTML = username;
+            document.getElementById("factcontent").innerHTML = info.fact;
+            document.getElementById("upvotecount").innerHTML = info.votes;
+            document.getElementById("downvotecount").innerHTML = info.downvotes;
+            document.getElementById("factusername").innerHTML = info.user;
             //document.getElementById("date").innerHTML = new Date(response.timestamp);
-            console.log(responseTxt);
+            //console.log(responseTxt);
         }
 
         if (statusTxt == "error") {
@@ -79,22 +37,18 @@ function saadaAjax() {
         $('#loadMoreButton').hide();
         $("#comment-1").load("/comment/next", function (responseTxt, statusTxt, xhr) {
             if (statusTxt == "success") {
-
-                var response = responseTxt.split(":");
-                var comments = response[1].split(",");
-                var comment = comments[0].substring(1, comments[0].length-2);
-                var names = response[2].split(",");
-                var name = names[0].substring(1, names[0].length-2);
+                var info = JSON.parse(responseTxt);
+                console.log(info);
                 document.getElementById("comment-1").innerHTML = "Kasutaja1 kommentaar";
 
                 $("#commentSection").append("<div class='row'>" +
                     "<div class='col-md-2'>" +
                         "<strong>" +
-                    name +
+                    info.name +
                     "</strong>" +
                             "</div>" +
                         "<div class='col-md-10'>" +
-                        comment +
+                        info.comment +
                         "</div>" +
                     "</div>");
 
