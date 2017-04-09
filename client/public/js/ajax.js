00b4b1c26779393df949f005ecee56c8b3a46076
nuubis <marttikakk@hotmail.com>
2017-04-09 23:27:11 +0300

    small fix

--- client/public/js/ajax.js
+++ client/public/js/ajax.js
@@ -42,7 +42,7 @@ function saadaAjax() {
                 $("#commentSection").append("<div class='row'>" +
                     "<div class='col-md-2'>" +
                         "<strong>" +
-                    info.name +
+                    info.user +
                     "</strong>" +
                             "</div>" +
                         "<div class='col-md-10'>" +
